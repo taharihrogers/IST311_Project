2ef2e8f9e750faed88d5d6f4da10f8712f7ab230
jmPRO20 <54557121+jmPRO20@users.noreply.github.com>
2019-10-14 20:13:55 -0400

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,27 @@
+/**
+	File name: Customer.java
+	Short description: ??????
+	IST 242 Assignment:  
+	@author Joshua Michaels
+	@version 1.01 2014-08-25
+	date of last revision:
+	details of the revision: none
+*/
+
+
+
+
+/**
+ *
+ * @author Joshua
+ */
+public class Customer {
+
+    private String customFName;
+    private String customLName;
+    
+    public Customer()
+    {
+        
+    }
+}
---
+++ FoodItem.java
@@ -0,0 +1,27 @@
+/**
+	File name: FoodItem.java
+	Short description: ??????
+	IST 242 Assignment:  
+	@author Joshua Michaels
+	@version 1.01 2014-08-25
+	date of last revision:
+	details of the revision: none
+*/
+
+
+
+
+/**
+ *
+ * @author Joshua
+ */
+public class FoodItem extends Submenu {
+
+    private String itemName;
+    private double price;
+    
+    public FoodItem()
+    {
+        
+    }
+}
